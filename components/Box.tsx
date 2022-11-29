a1b2642c97d6d854f511021980e6c64ef554bfc7
dwiki.arlan <dwiki.arlan@ruangguru.com>
2022-11-29 23:35:46 +0700

    fix white base

--- components/Box.tsx
+++ components/Box.tsx
@@ -7,7 +7,7 @@ const Box = ({
 }: HTMLAttributes<HTMLDivElement>) => {
   return (
     <div
-      className={`w-full bg-white p-4 rounded-[15px] shadow-sm ${
+      className={`w-full bg-white-base p-4 rounded-[15px] shadow-sm ${
         className ?? ""
       }`}
       {...rest}
